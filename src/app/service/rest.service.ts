b405b708cbb609ff45113abadb9c935d3ea897b8
Essleman-se <seydo.ess@gmail.com>
2022-01-16 10:24:10 -0800

    chnges of observable

--- src/app/service/rest.service.ts
+++ src/app/service/rest.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 
 const httpOptions = {
@@ -10,11 +12,17 @@ const httpOptions = {
 })
 export class RestService {
 
+  private apiServerUrl = environment.apiBaseUrl;
+
   constructor(private _httpClient: HttpClient) { }
 
   getAllcustomers_url:string = "/customer/all"; //load list of customers
 
-  getCustomerList(){
-    return this._httpClient.post(this.getAllcustomers_url, httpOptions);
+  // getCustomerList(){
+  //   return this._httpClient.get(this.getAllcustomers_url, httpOptions);
+  // }
+
+  public getCustomerList(): Observable<any[]> {
+    return this._httpClient.post<any[]>(`${this.apiServerUrl}/customer/all`, String);
   }
 }
